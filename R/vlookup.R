a572540712cd58d8bc0ef05e643a89133ec7f3b3
Christophe Gigot <ch.gigot@gmail.com>
2018-01-28 23:47:55 +0100

    Add an example for vlookup function.

--- R/vlookup.R
+++ R/vlookup.R
@@ -11,6 +11,19 @@
 #' @param near_val Not yet implemented.
 #' @param sort_order Not yet implemented.
 #'
+#' @examples
+#' shopping <- data.frame(product  = c("Sports Almanac", "Frisbee",
+#'                                     "Water bottles"),
+#'                        quantity = c(1, 3, 6))
+#' seller <- data.frame(product  = c("Frisbee", "Lava lamp", "Sports Almanac",
+#'                                   "Video camera", "Water bottles"),
+#'                      price    = c(7, 20, 35, 75, 3))
+#' shopping$unit_price <- vlookup(shopping$product, seller, "price")
+#' shopping$total_price <- with(shopping, unit_price * quantity)
+#' seller
+#' shopping
+#' sum(shopping$total_price)
+#'
 #' @export
 #------------------------------------------------------------------------------#
 vlookup <- function(x, table, col_return, ..., col_search = 1) {
